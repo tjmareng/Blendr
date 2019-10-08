2fe1d257b8d78a975e84a65d0368344b53e4826e
Javen Zamojcin <javen.william@gmail.com>
2019-10-08 18:55:22 -0400

    removed firebase config from views.py, and moved into settings.py

--- Blendr/settings.py
+++ Blendr/settings.py
@@ -11,10 +11,10 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import pyrebase
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -26,7 +26,6 @@ SECRET_KEY = '+$*vp0)8)a(54&o9p!!&&5g+t7@k2fj7#_cw0+wfgqwd!j2xmx'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -54,7 +53,7 @@ ROOT_URLCONF = 'Blendr.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],        'APP_DIRS': True,
+        'DIRS': [], 'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
@@ -68,17 +67,19 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'Blendr.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+firebaseConfig = {
+    "apiKey": "AIzaSyDmv7TUeHMoLSz1sG5a38iE4SdZPBY08Oo",
+    "authDomain": "blendr-c1bd5.firebaseapp.com",
+    "databaseURL": "https://blendr-c1bd5.firebaseio.com",
+    "storageBucket": "blendr-c1bd5.appspot.com",
+    "projectId": "blendr-c1bd5",
+    "appId": "1:289396306071:web:fe685d0d1278f3c76df9d2"
 }
 
+firebase = pyrebase.initialize_app(firebaseConfig)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
@@ -98,7 +99,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
@@ -111,7 +111,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
