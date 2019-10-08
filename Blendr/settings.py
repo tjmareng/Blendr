f2cc9928a64c51cc230badb27febe8eafe5bc27d
Javen Zamojcin <javen.william@gmail.com>
2019-10-08 18:40:20 -0400

    added firebase info to views.py

--- Blendr/settings.py
+++ Blendr/settings.py
@@ -54,8 +54,7 @@ ROOT_URLCONF = 'Blendr.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
+        'DIRS': [],        'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
--- main/views.py
+++ main/views.py
@@ -1,3 +1,13 @@
+import pyrebase
 from django.shortcuts import render
 
-# Create your views here.
+config = {
+    "apiKey": "AIzaSyDmv7TUeHMoLSz1sG5a38iE4SdZPBY08Oo",
+    "authDomain": "blendr-c1bd5.firebaseapp.com",
+    "databaseURL": "https://blendr-c1bd5.firebaseio.com",
+    "storageBucket": "blendr-c1bd5.appspot.com",
+    "projectId": "blendr-c1bd5",
+    "appId": "1:289396306071:web:fe685d0d1278f3c76df9d2"
+}
+
+firebase = pyrebase.initialize_app(config)
