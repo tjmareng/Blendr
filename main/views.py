0a1f2158ccb3664932a9169a7a1e8c20c2087cfc
Javen Zamojcin <javen.william@gmail.com>
2019-10-22 18:31:05 -0400

    Updated views.py functions

--- main/views.py
+++ main/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 
 from Blendr.firebase_config import db, auth
-# from .models import User
 from main.models import User
 
 
@@ -10,31 +9,42 @@ def goto_index(request):
 
 
 def goto_login(request):
-    # after finishing registration
-    if request.method == 'POST':
-        biography = request.POST.get('paragraph_text')
-        sexuality = request.POST.get('sexuality')
-        gender = request.POST.get('gender')
-
     return render(request, 'main/login.html')
 
 
 def goto_profile_creation(request):
     if request.method == 'POST':
-        username = request.POST.get('Username')
         email = request.POST.get('Email')
         password = request.POST.get('Password')
         conf_password = request.POST.get('confirmPassword')
 
         if password == conf_password:
             auth.create_user_with_email_and_password(email, password)
-            new_user = User(email, username)
-            db.child("users").child(username).set(user_to_dict(new_user))
 
             return render(request, 'main/profileCreation.html')
         # else: handle not matching error
 
 
+def goto_complete_registration(request):
+    if request.method == 'POST':
+        # after finishing registration
+
+        username = request.POST.get('Username')
+        email = request.POST.get('Email')
+        biography = request.POST.get('paragraph_text')
+        sexuality = request.POST.get('sexuality')
+        gender = request.POST.get('gender')
+
+        new_user = User(email, username)
+        db.child("users").child(email).set(user_to_dict(new_user))
+
+    return render(request, '')
+
+
+def reset_password(request):
+    return request(request, '')
+
+
 # Converts User object to a dictionary to be stored in DB
 def user_to_dict(self):
-    return {"name": self.name, "email": self.email, }
+    return {"name": self.username, "email": self.email, }
